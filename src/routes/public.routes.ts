5ff13778f73abedddfb2b1586a069f32eda19509
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2023-02-25 01:43:47 -0500

    Update public.routes.ts

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { PublicController } from "../controllers/public.controller";
 
 const publicController = new PublicController();
-
 export const publicRoutes: Router = Router();
 publicRoutes.get('/contact', publicController.sendContactPage);
 publicRoutes.get('/*', publicController.sendErrorPage);
